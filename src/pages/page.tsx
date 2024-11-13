a1b977647625488d76664daa05d2493988544624
karthikraja <datasackseodev@gmail.com>
2024-11-13 14:49:43 +0530

    add footer page

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,6 +1,7 @@
 import AnalyticsPage from "@/components/Pages/Analytics"
 import BenefitsPage from "@/components/Pages/Benefits"
 import FAQPage from "@/components/Pages/FAQ"
+import Footer from "@/components/Pages/Footer"
 import HomePage from "@/components/Pages/Hero"
 import ManageServicePage from "@/components/Pages/ManageService"
 import PricingPage from "@/components/Pages/Pricing"
@@ -13,6 +14,7 @@ const IndexPage = () => {
   <AnalyticsPage />
   <ManageServicePage/>
   <FAQPage/>
+  <Footer/>
   </div>
 }
 
