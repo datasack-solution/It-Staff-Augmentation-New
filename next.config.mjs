eb14bba4d3fa978bf34bc0cca5cdc006064adda5
karthikraja <datasackseodev@gmail.com>
2025-05-20 14:43:03 +0530

    comment output:export

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ export default withTM([
     unoptimized:true,
     domains: ['bunny-wp-pullzone-dgpgtislbk.b-cdn.net'], // Replace with your image domains
   },
-  output:'export',
+  // output:'export',
   // basePath: '', // Ensure no subdirectory prefix is added
   // assetPrefix: '', // Make asset paths relative
   assetPrefix: '', // Adjust assets for cPanel subdirectory
