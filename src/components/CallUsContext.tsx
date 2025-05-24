fe15d2842297d7925511dea43ce3f31446fb85a6
karthikraja <datasackseodev@gmail.com>
2025-05-24 15:26:01 +0530

    arrange the code and remove unnessacery import'i

--- src/components/CallUsContext.tsx
+++ src/components/CallUsContext.tsx
@@ -3,23 +3,23 @@ import { createContext, useContext, useEffect, useState } from "react"
 interface CallUsModalContextProps {
   setIsCallUsModalOpen: React.Dispatch<React.SetStateAction<CallUsModalStateType>>
   isCallUsModalOpen: CallUsModalStateType
-  isOpened:boolean
-  darkMode:boolean,
-  setDarkMode:React.Dispatch<React.SetStateAction<boolean>>
-  quantities:TechQuantitiesType
-  setQuantities:React.Dispatch<React.SetStateAction<TechQuantitiesType>>,
-  setDuration:React.Dispatch<React.SetStateAction<string>>,
-  duration:string
-  isPricingToastOpened:boolean
+  isOpened: boolean
+  darkMode: boolean,
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>
+  quantities: TechQuantitiesType
+  setQuantities: React.Dispatch<React.SetStateAction<TechQuantitiesType>>,
+  setDuration: React.Dispatch<React.SetStateAction<string>>,
+  duration: string
+  isPricingToastOpened: boolean
 }
 
 const CallUsModalContext = createContext<CallUsModalContextProps | undefined>(undefined);
 
 type CallUsModalStateType = {
-  isOpen:boolean,
-  hasTechs:boolean,
-  techLabels:string[] | null,
-  techQuantities:number[]| null
+  isOpen: boolean,
+  hasTechs: boolean,
+  techLabels: string[] | null,
+  techQuantities: number[] | null
 }
 
 export type TechQuantitiesType = { [category: string]: { [tech: string]: number } }
@@ -27,10 +27,10 @@ let isPricingToastOpened = false
 
 export const CallUsModalProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isCallUsModalOpen, setIsCallUsModalOpen] = useState<CallUsModalStateType>({
-    isOpen:false,
-    hasTechs:false,
-    techLabels:[],
-    techQuantities:[]
+    isOpen: false,
+    hasTechs: false,
+    techLabels: [],
+    techQuantities: []
   })
   const [quantities, setQuantities] = useState<TechQuantitiesType>({});
   const [duration, setDuration] = useState<string>('6 months');
@@ -39,19 +39,19 @@ export const CallUsModalProvider: React.FC<{ children: React.ReactNode }> = ({ c
   const [isOpened, setIsOpened] = useState(false)
   const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     setIsOpened(false)
-  },[])
+  }, [])
 
   useEffect(() => {
-    const handleBeforeUnload = (event:BeforeUnloadEvent) => {
-  const techs = Object.values(quantities).flatMap(r => { return Object.keys(r) })
-        if (techs.length>0) {
-            isPricingToastOpened=true
-            event.preventDefault();
-            event.returnValue = "Are you sure you want to leave this page?"; //Modern browsers (Chrome 51+, Safari 9.1+, Firefox 48+) ignore the returnValue property and do not display a confirmation dialog.
-            //Legacy browsers (Internet Explorer, older versions of Chrome and Safari) may still display a confirmation dialog if returnValue is set.
-        }
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      const techs = Object.values(quantities).flatMap(r => { return Object.keys(r) })
+      if (techs.length > 0) {
+        isPricingToastOpened = true
+        event.preventDefault();
+        event.returnValue = "Are you sure you want to leave this page?"; //Modern browsers (Chrome 51+, Safari 9.1+, Firefox 48+) ignore the returnValue property and do not display a confirmation dialog.
+        //Legacy browsers (Internet Explorer, older versions of Chrome and Safari) may still display a confirmation dialog if returnValue is set.
+      }
     };
 
     // Add the event listener
@@ -59,11 +59,11 @@ export const CallUsModalProvider: React.FC<{ children: React.ReactNode }> = ({ c
 
     // Cleanup the event listener on component unmount
     return () => {
-        isPricingToastOpened=true
-        window.removeEventListener("beforeunload", handleBeforeUnload);
+      isPricingToastOpened = true
+      window.removeEventListener("beforeunload", handleBeforeUnload);
     };
 
-}, [quantities]);
+  }, [quantities]);
 
   useEffect(() => {
     if (isCallUsModalOpen.isOpen) {
@@ -79,7 +79,7 @@ export const CallUsModalProvider: React.FC<{ children: React.ReactNode }> = ({ c
   }, [isCallUsModalOpen.isOpen]);
 
   return (
-    <CallUsModalContext.Provider value={{ isCallUsModalOpen, setIsCallUsModalOpen, isOpened , darkMode,setDarkMode,quantities,setQuantities,duration,setDuration, isPricingToastOpened}}>
+    <CallUsModalContext.Provider value={{ isCallUsModalOpen, setIsCallUsModalOpen, isOpened, darkMode, setDarkMode, quantities, setQuantities, duration, setDuration, isPricingToastOpened }}>
       {children}
     </CallUsModalContext.Provider>
   );
